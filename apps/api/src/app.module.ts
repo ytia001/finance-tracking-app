3291ff830d9f5c0cac0cd16085a46c21419c3bf7
ytia001 <yiken36@gmail.com>
2026-04-12 10:00:35 +0800

    implement auth backend

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DrizzleModule } from './db/db.module';
 import { EntriesModule } from './entries/entries.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DrizzleModule, EntriesModule]
+  imports: [DrizzleModule, EntriesModule, AuthModule]
 })
 export class AppModule {}
---
+++ apps/api/src/auth/dto/auth.dto.ts
@@ -0,0 +1,20 @@
+import { IsString, IsEmail, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(8)
+  password!: string;
+
+  @IsString()
+  firstName!: string;
+
+  @IsString()
+  lastName!: string;
+}
+
+export class LoginResponseDto {
+  accessToken!: string;
+}
